cbbe8afcbb2fe082ff4be3e047c4882922914559
Matteo Barbero <100956796+Teleteo23@users.noreply.github.com>
2022-03-10 17:02:50 +0100

    Update R_code_rs.r

--- R_code_rs.r
+++ R_code_rs.r
@@ -1,3 +1,8 @@
 # questo è il primo script che useremo a lezione
 
+#Install.packages ("raster")
 library(raster)
+
+#Setto la cartella dei dati di lavoro (scelgo un percorso molto breve)
+setwd("C:/lab/") # Windows
+
