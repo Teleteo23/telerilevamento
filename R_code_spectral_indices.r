ebddc0821409bd58517ad893bdb11fe3df99ed6f
Matteo Barbero <100956796+Teleteo23@users.noreply.github.com>
2022-03-31 17:47:04 +0200

    Update R_code_spectral_indices.r

--- R_code_spectral_indices.r
+++ R_code_spectral_indices.r
@@ -74,3 +74,66 @@ difdvi <- dvi1992 - dvi2006
 cld <- colorRampPalette(c('blue','white','red'))(100) 
 plot(difdvi, col=cld, main=" Differenza del DVI tra il 1992 e il 2006")
 
+# Il range del DVI a 8 bit va da -255 a 255
+# Il range del NDVI a 8 bit va da -1 a 1
+# Il range del DVI a 16 bit va da -65535 a 65535
+# Il range del NDVI a 16 bit va da -1 a 1
+# E' meglio utilizzare l'NDVI perché è possibile fare un confronto tra immagini con risoluzione radiometrica differente
+
+# Calcolo l'NDVI per il 1992
+ndvi1992 <- (L1992[[1]] - L1992[[2]])/(L1992[[1]] + L1992[[2]])
+
+# Chiamo l'oggetto per visualizzarne le informazioni
+
+ndvi1992
+
+# Plotto il NDVI del 1992
+
+plot(ndvi1992, col=cl)
+
+# Costruisco un multiframe 
+# Sopra metto l'immagine del 1992 (linear stretch)
+# Sotto metto l'NDVI del 1992
+par(mfrow=c(2,1))
+plotRGB(L1992, r=1, g=2, b=3, stretch="lin")
+plot(ndvi1992, col=cl)
+#dev.off()
+
+# Calcolo l'NDVI per il 2006
+ndvi2006 <- (L2006[[1]] - L2006[[2]])/(L2006[[1]] + L2006[[2]])
+
+# Chiamo l'oggetto per visualizzarne le informazioni
+
+ndvi2006
+
+# Plotto il NDVI del 2006
+
+plot(ndvi2006, col=cl)
+
+# Costruisco un multiframe 
+# Sotto metto l'NDVI del 1992
+# Sotto metto l'NDVI del 2006
+par(mfrow=c(2,1))
+plot(ndvi1992, col=cl)
+plot(ndvi2006, col=cl)
+dev.off()
+
+# Carico il pacchetto "RStoolbox" (vedi ad inizio script)
+
+# Calcolo gl'indici spettrali dell'immagine del 1992 attraverso la funzione "spectralIndices"
+si1992 <- spectralIndices(L1992, green=3, red=2, nir=1)
+
+# Plotto l'si del 1992
+plot(si1992, col=cl)
+
+# Calcolo gl'indici spettrali dell'immagine del 2006 attraverso la funzione "spectralIndices"
+si2006 <- spectralIndices(L2006, green=3, red=2, nir=1)
+
+# Plotto l'si del 2006
+plot(si2006, col=cl)
+
+# Installo e/o carico il pacchetto "rasterdiv" (vedi ad inizio script)
+
+#
+
+plot(copNDVI)
