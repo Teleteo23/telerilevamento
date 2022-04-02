8b1f9c41e303e1a22bfd30a236154d32d87fd759
Matteo Barbero <100956796+Teleteo23@users.noreply.github.com>
2022-04-02 12:45:37 +0200

    Update R_code_spectral_indices.r

--- R_code_spectral_indices.r
+++ R_code_spectral_indices.r
@@ -34,8 +34,9 @@ plotRGB(L1992, r=1, g=2, b=3, stretch="lin")
 plotRGB(L2006, r=1, g=2, b=3, stretch="lin")
 dev.off()
 
-# Calcolo il difference vegetation index (differenza tra le riflettanze nel NIR e ner rosso)
+# Calcolo il difference vegetation index(DVI)(differenza tra le riflettanze nel NIR e nel rosso)
 # Il calcolo lo faccio sia per il 1992 che per il 2006
+
 # 1992
 dvi1992 <- L1992$defor1.1 - L1992$defor1.2
 
@@ -48,27 +49,30 @@ plot(dvi1992)
 # Creo una nuova scala colori per miglirare la visualizzazione
 cl <- colorRampPalette(c('darkblue','yellow','red','black'))(100) # specifying a color scheme
 
+# Plotto il DVI del 1992 con la nuova scala colori
 plot(dvi1992, col=cl, main="DVI at 1992")
 
 # 2006
 dvi2006 <- L2006$defor2.1 - L2006$defor2.2
 
+# Chiamo l'oggetto per visualizzarne le informazioni
 dvi2006
 
+# Plotto il DVI del 2006 con la nuova scala colori
 plot(dvi2006, col=cl, main="DVI at 2006")
 
+# Creo un multiframe con il DVI del 1992 e del 2006
 par(mfrow=c(2,1))
 plot(dvi1992, col=cl, main="DVI at 1992")
 plot(dvi2006, col=cl, main="DVI at 2006")
 dev.off()
 
-# DVI nel tempo
+# Calcolo la differenza del DVI nel tempo
 difdvi <- dvi1992 - dvi2006
 
-# dev.off()
-
+# Creo una nuova scala colori e plotto la differenza del DVI calcolata
 cld <- colorRampPalette(c('blue','white','red'))(100) 
-plot(difdvi, col=cld, main=" Differenza del DVI tra il 1992 e il 2006")
+plot(difdvi, col=cld, main="Differenza del DVI tra il 1992 e il 2006")
 
 # Il range del DVI a 8 bit va da -255 a 255
 # Il range del NDVI a 8 bit va da -1 a 1
@@ -80,15 +84,13 @@ plot(difdvi, col=cld, main=" Differenza del DVI tra il 1992 e il 2006")
 ndvi1992 <- (L1992[[1]] - L1992[[2]])/(L1992[[1]] + L1992[[2]])
 
 # Chiamo l'oggetto per visualizzarne le informazioni
-
 ndvi1992
 
 # Plotto il NDVI del 1992
-
 plot(ndvi1992, col=cl)
 
 # Costruisco un multiframe 
-# Sopra metto l'immagine del 1992 (linear stretch)
+# Sopra metto l'immagine del 1992 in RGB (linear stretch)
 # Sotto metto l'NDVI del 1992
 par(mfrow=c(2,1))
 plotRGB(L1992, r=1, g=2, b=3, stretch="lin")
@@ -99,11 +101,9 @@ plot(ndvi1992, col=cl)
 ndvi2006 <- (L2006[[1]] - L2006[[2]])/(L2006[[1]] + L2006[[2]])
 
 # Chiamo l'oggetto per visualizzarne le informazioni
-
 ndvi2006
 
 # Plotto il NDVI del 2006
-
 plot(ndvi2006, col=cl)
 
 # Costruisco un multiframe 
@@ -129,7 +129,5 @@ si2006 <- spectralIndices(L2006, green=3, red=2, nir=1)
 plot(si2006, col=cl)
 
 # Installo e/o carico il pacchetto "rasterdiv" (vedi ad inizio script)
-
 #
-
 plot(copNDVI)
