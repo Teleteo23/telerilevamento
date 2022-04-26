1f72646106f3244577f58bb093dc921d0b53d4d2
Matteo Barbero <100956796+Teleteo23@users.noreply.github.com>
2022-04-26 16:58:31 +0200

    Update R_code_classification.r

--- R_code_classification.r
+++ R_code_classification.r
@@ -29,3 +29,40 @@ SOc
 cl <- colorRampPalette(c('yellow','black','red'))(100)
 plot(SOc$map, col=cl)
 dev.off()
+
+# Importo un anuova immagine
+# Utilizzo la funzione "brick" per importare l'immagine e la assegno ad un oggetto
+# Utilizzo la funzione "brick" perché l'immagine è un RGB
+GC <- brick("dolansprings_oli_2013088_canyon_lrg.jpg")
+# Chiamo l'oggetto per visualizzarne le informazioni
+GC
+
+# Essendo un immagine RGB, faccio un plot RGB 
+plotRGB(GC, 1, 2, 3, stretch="lin")
+plotRGB(GC, 1, 2, 3, stretch="hist")
+dev.off()
+
+# Classifico l'immagine sulla base di come si sono disposti i pixel nello spazio delle tre bande a nostra disposizione
+# Per farlo utilizzo la funzione "unsuperClass" indicando il numero di classi che voglio e la assegno ad un oggetto
+GCc2 <- unsuperClass(GC, nClasses=2)
+# Chiamo l'oggetto per visualizzarne le informazioni
+GCc2
+# Plotto l'immagine classificata legandola alla mappa creata durante la classificazione
+plot(GCc2$map)
+dev.off()
+
+# Faccio una nuova classificazione aumentando il numero di classi
+GCc4 <- unsuperClass(GC, nClasses=4)
+# Chiamo l'oggetto per visualizzarne le informazioni
+GCc4
+# Plotto l'immagine classificata legandola alla mappa creata durante la classificazione
+# Creo una nuova scala colori per miglirare la visualizzazione
+cl2 <- colorRampPalette(c('yellow','red','blue','black'))(100)
+plot(GCc4$map, col=cl2)
+dev.off()
+
+# Comparo la mappa classificata con quella originale
+par(mfrow=c(2,1))
+plot(GCc4$map, col=cl2)
+plotRGB(GC, 1, 2, 3, stretch="hist")
+dev.off()
