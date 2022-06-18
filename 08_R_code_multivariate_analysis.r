136d065a6b24a4dce672c11491bc78732207fd60
Matteo Barbero <100956796+Teleteo23@users.noreply.github.com>
2022-06-18 13:10:13 +0200

    Update and rename R_code_multivariate_analysis.r to 08_R_code_multivariate_analysis.r

--- 08_R_code_multivariate_analysis.r
+++ 08_R_code_multivariate_analysis.r
@@ -1,4 +1,5 @@
 # Questo è l'ottavo script che useremo a lezione
+# R_code_multivariate_analysis.r
 
 # Carico il pacchetto raster
 library(raster)
@@ -38,3 +39,45 @@ g1 <- ggRGB(I11, 4, 3, 2)
 g2 <- ggRGB(I11res, 4, 3, 2)
 g3 <- ggRGB(I11res2, 4, 3, 2)
 g1/g2/g3
+
+# PCA analysis
+# Faccio la PCA dell'oggetto appena ricampionato
+# Per farlo utilizzo la funzione rasterPCA e la assegno ad un ogggetto
+I11respca <- rasterPCA(I11res)
+
+# Faccio il riassunto dell'oggetto appena creato attraverso la funzione summary
+summary(I11respca$model)
+
+# Faccio il plot della PCA
+plot(I11respca$map)
+
+# Faccio una serie di ggplot utilizzando due diversi elementi della PCA e due mappe differenti dell'immagine ricampionata
+
+g4 <- ggplot()+
+geom_raster(I11respca$map, mapping =aes(x=x, y=y, fill=PC1))+
+scale_fill_viridis(option = "inferno") +
+ggtitle("PC1")
+
+g5 <-ggplot()+
+geom_raster(I11respca$map, mapping =aes(x=x, y=y, fill=PC7))+
+scale_fill_viridis(option = "inferno") +
+ggtitle("PC7")
+
+g4+g5
+
+g6 <-ggplot()+
+geom_raster(I11res, mapping =aes(x=x, y=y, fill=B4_sre))+
+scale_fill_viridis(option = "inferno") +
+ggtitle("NIR")
+
+g4+g6
+
+g7 <- ggRGB(I11res, 2, 3, 4, stretch="hist")
+
+g4+g7
+
+# Faccio due plot RGB differenti di tre livelli della PCA
+plotRGB(I11respca$map, 1, 2, 3, stretch="lin")
+plotRGB(I11respca$map, 5, 6, 7, stretch="lin")
+
+ 
