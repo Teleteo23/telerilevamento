48d590af9d4d67eef5d520c418cd355b96cb3411
Matteo Barbero <100956796+Teleteo23@users.noreply.github.com>
2022-06-18 11:47:12 +0200

    Update and rename R_code_classification.r to 05_R_code_classification.r

--- 05_R_code_classification.r
+++ 05_R_code_classification.r
@@ -1,4 +1,5 @@
 # Questo è il quinto script che useremo a lezione
+# R_code_classification.r
 
 # Carico il pacchetto raster
 library(raster)
@@ -15,6 +16,7 @@ SO <- brick("Solar_Orbiter_s_first_views_of_the_Sun_pillars.jpg")
 SO
 
 # Essendo un immagine RGB, faccio un plot RGB 
+# Utilizzo sia lo stretch lineare che a istogramma
 plotRGB(SO, 1, 2, 3, stretch="lin")
 plotRGB(SO, 1, 2, 3, stretch="hist")
 dev.off()
@@ -30,7 +32,7 @@ cl <- colorRampPalette(c('yellow','black','red'))(100)
 plot(SOc$map, col=cl)
 dev.off()
 
-# Importo un anuova immagine
+# Importo una nuova immagine
 # Utilizzo la funzione "brick" per importare l'immagine e la assegno ad un oggetto
 # Utilizzo la funzione "brick" perché l'immagine è un RGB
 GC <- brick("dolansprings_oli_2013088_canyon_lrg.jpg")
@@ -52,6 +54,8 @@ plot(GCc2$map)
 dev.off()
 
 # Faccio una nuova classificazione aumentando il numero di classi
+# Se voglio mantenere le stesse classi ogni volta che faccio questa classificazione utilizzo il comando "set.seed()"
+set.seed(1)
 GCc4 <- unsuperClass(GC, nClasses=4)
 # Chiamo l'oggetto per visualizzarne le informazioni
 GCc4
