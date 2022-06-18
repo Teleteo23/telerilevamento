1433252e16f42422452aafc80f47b5e6680f39ed
Matteo Barbero <100956796+Teleteo23@users.noreply.github.com>
2022-06-18 14:37:09 +0200

    Update 13_R_code_functions.r

--- 13_R_code_functions.r
+++ 13_R_code_functions.r
@@ -1,4 +1,5 @@
 # Questo è il tredicesimo script che useremo a lezione
+# R_code_functions.r
 
 library(raster)
 # Setto la cartella dei dati di lavoro (scelgo un percorso molto breve)
@@ -6,6 +7,8 @@ setwd("C:/lab/") # Windows
 
 # Faremo una serie di funzioni
 
+# Funzione 1
+# Creo una funzione che, una volta chiamata, "saluta" il nome inserito
 cheer_me <- function(your_name) {
 cheer_string <- paste("Hello", your_name, sep = " ")
 print(cheer_string)
@@ -13,7 +16,8 @@ print(cheer_string)
 
 cheer_me("matteo")
 
-
+# Funzione 2
+# Creo una funzione che, una volta chiamata, "saluta" il nome inserito n volte, dove "n" lo scelgo io
 cheer_me_n_times <- function(your_name, n) {
 cheer_string <- paste("Hello", your_name, sep = " ")
 
@@ -24,6 +28,9 @@ print(cheer_string)
 
 cheer_me_n_times("matteo", 5)
 
+
+# Funzione 3
+# Creo una funzione che, risciamata un'immagine dalla cartella di lavoro, posso scegliere se plottarla con una palette creata da me o lasciare quella di default
 dato <- raster("sentinel.png")
 
 plot(dato)
